0a8dfa70161a2dba9df0b98a1589ff2f7bbd36f1
Natalia <6568973@gmail.com>
2018-10-16 15:56:56 -0400

    new commit-m
    new commit-m

---
+++ lesson3/class_station.rb
@@ -0,0 +1,21 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def pass_train(train)
+    trains << train
+  end
+
+  def send_train(train)
+    trains.delete(train)
+  end
+
+  def train_list_by_type(type)
+    trains.count { |train| train.type == type }
+  end
+end
+
